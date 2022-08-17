25181d1bfb157ea387779d8b3acba100a019069f
caiodjo <caiodjo.fernandes@gmail.com>
2022-08-17 00:17:55 -0400

    new logic implement

--- tratamento.js
+++ tratamento.js
@@ -1,90 +1,62 @@
 const cavalo = [2,2];
 const alvo = [2,5];
-const movimentos = [[cavalo],[],[],[],[],[],[],[],[],[],[],[],[],[],[]];
+const movimentos = [];
 const jaPassei = [[]];
-let moduloAlv = 0, moduloCav = 0;
 
+let moduloAlv = 0, moduloCav = 0;
 function distanciaPecas (peca, alvo) {
     moduloCav = peca.reduce((val, acc) => val+acc,0);
     moduloAlv = alvo.reduce((val, acc) => val+acc,0);
 }
-function moverCavalo(array, movLin, movCol,i) {
+function moverCavalo(array, movLin, movCol) {
     if ((array[0] + movLin < 7 && array[0] + movLin >= 0) && (array[1] + movCol < 7 && array[1] + movCol >= 0)) {
+        const arr =[]
         const coordX = array[0] + movLin;
         const coordY = array[1] + movCol;
-        movimentos[i].push([coordX, coordY]);
+        arr.push(coordX);
+        arr.push(coordY);
+        return arr
     }
 }
-function maisLonge(){
-    if(distNov > 3){
-    }
-}
-function gerarPossibilidades(coordedanas,c){
-    const multiversoC=coordedanas;
-    const multiversoA=alvo;
-    let movCol, movLin;
-    for (let i = 0; i < 8; i++) {
-        switch (i) {
-            case 0:
-                movLin = -2;
-                movCol = -1;
-                moverCavalo(multiversoC, movLin, movCol,c);
-                break;
-            case 1:
-                movLin = -1;
-                movCol = -2;
-                moverCavalo(multiversoC, movLin, movCol,c);
-                break;
-            case 2:
-                movLin = 1;
-                movCol = -2;
-                moverCavalo(multiversoC, movLin, movCol,c);
-                break;
-            case 3:
-                movLin = 2;
-                movCol = -1;
-                moverCavalo(multiversoC, movLin, movCol,c);
-                break;
-            case 4:
-                movLin = 2;
-                movCol = 1;
-                moverCavalo(multiversoC, movLin, movCol,c);
-                break;
-            case 5:
-                movLin = 1;
-                movCol = 2;
-                moverCavalo(multiversoC, movLin, movCol,c);
-                break;
-            case 6:
-                movLin = -1;
-                movCol = 2;
-                moverCavalo(multiversoC, movLin, movCol,c);
-                break;
-            case 7:
-                movLin = -2;
-                movCol = 1;
-                moverCavalo(multiversoC, movLin, movCol,c);
-                break;
-            default:
-                console.log('Vasco da gama');
-        }
-    }
-}
+
 function caminhosPossiveis(array){
     const arrayResultado = [];
-    arrayResultado.push()
-}
-function loopAteAchar(arrayDeCoordenadas,passado){
-    
-}
-
-gerarPossibilidades(cavalo,0);
-let cont = 1
-movimentos[0].forEach(element => {
-    if(element != [[2,2]]){
-        gerarPossibilidades(element,cont);
-        cont++;
+    if(moverCavalo(array, -2,-1)){
+        arrayResultado.push(moverCavalo(array, -2,-1));
     }
-});
+    if(moverCavalo(array, -1,-2)){
+        arrayResultado.push(moverCavalo(array, -1,-2));
+    }
+    if(moverCavalo(array, 1,-2)){
+        arrayResultado.push(moverCavalo(array, 1,-2));
+    }
+    if(moverCavalo(array, 2,-1)){
+        arrayResultado.push(moverCavalo(array, 2,-1));
+    }
+    if(moverCavalo(array, 2,1)){
+        arrayResultado.push(moverCavalo(array, 2,1));
+    }
+    if(moverCavalo(array, 1,2)){
+        arrayResultado.push(moverCavalo(array, 1,2));
+    }
+    if(moverCavalo(array, -1,2)){
+        arrayResultado.push(moverCavalo(array, -1,2));
+    }
+    if(moverCavalo(array, -2,1)){
+        arrayResultado.push(moverCavalo(array, -2,1));
+    }
+    return arrayResultado;
+}
 
-console.log(movimentos);
+movimentos.push(cavalo);
+movimentos.push(caminhosPossiveis(cavalo));
+function interagindo (array){
+    const arrReturn = [];
+    array.forEach(element => {
+        arrReturn.push(caminhosPossiveis(element))
+    });
+    //console.log(arrReturn);
+    return arrReturn
+}
+movimentos.push(interagindo(movimentos[1]));
+console.log(movimentos[2]);
